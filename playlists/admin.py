46a29ce4a3a1f24fb53661776403f4797c58794e
jyouj <youj.whenever.prime3@gmail.com>
2019-01-03 18:51:32 +0900

    sortable

--- playlists/admin.py
+++ playlists/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
-
+from adminsortable.admin import SortableAdmin
 from .models import Title, Song
 
 # Register your models here.
-admin.site.register(Title)
-admin.site.register(Song)
+class TitleAdmin(SortableAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+
+class SongAdmin(SortableAdmin):
+    list_display = ('id', 'song_name', 'title')
+    list_display_links = ('id', 'song_name')
+
+
+admin.site.register(Title, TitleAdmin)
+admin.site.register(Song, SongAdmin)
--- playlists/models.py
+++ playlists/models.py
@@ -1,27 +1,33 @@
 from django.db import models
 import uuid
+from adminsortable.models import SortableMixin
+from adminsortable.fields import SortableForeignKey
 
 # Create your models here.
-class Title(models.Model):
+class Title(SortableMixin):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=50)
     date = models.CharField(max_length=30)
     created_at = models.DateTimeField(auto_now_add=True)
+    the_order = models.PositiveIntegerField(default=0, editable=False, db_index=True)
 
     class Meta:
-        ordering = ['-created_at']
+        ordering = ['the_order']
 
     def __str__(self):
         return self.name
 
-class Song(models.Model):
-    title = models.ForeignKey(Title, on_delete=models.CASCADE)
+class Song(SortableMixin):
+    title = SortableForeignKey(Title, on_delete=models.CASCADE)
     turn = models.CharField(max_length=30)
     song_name = models.CharField(max_length=50)
     artist = models.CharField(max_length=30)
     youtube = models.CharField(max_length=150, blank=True, null=True)
     link = models.CharField(max_length=150, blank=True, null=True)
+    the_order = models.PositiveIntegerField(default=0, editable=False, db_index=True)
+
+    class Meta:
+        ordering = ['the_order']
 
     def __str__(self):
         return self.song_name
-    
